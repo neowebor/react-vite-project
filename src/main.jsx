de7004b1ae5f56a494b5564eec8ab6fdc1c71c5c
neowebor <neowebor@gmail.com>
2024-11-24 15:49:31 +0200

    Add Class Component example

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,41 @@
 import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-const elem = React.createElement('h1', {}, 'hello from react')
+/*
+  Компоненти бувають двох видів:
+    - класові
+    - функціональні
+*/
+
+// Класовий компонент
+class MyClassComponent extends React.Component {
+  render() {
+    // має повертати якусь річ яку реакт буде відмальовувати на його місці
+    /*
+      реакт малює:
+        цифри
+        непусті рядки
+        реакт-елементи
+        інші реакт компоненти
+        в залежності від вмісту може відмальовувати масиви
+      реакт НЕ відображає:  
+        булеві значення
+        null
+        undefined
+        Symbol
+      реакт буде кидати помилку якщо змусити його відмалювати звичайний об'єкт
+    */
+    
+    const heading = React.createElement('h2', {className: 'articleHeading'}, 'Class Component heading');
+
+    const textPar = React.createElement('p', null, 'Lorem Ipsum')
+
+    const article = React.createElement('article', null, heading, textPar)
+    
+    return article;
+  }
+}
+
+const elem = React.createElement(MyClassComponent)
 
 createRoot(document.getElementById('root')).render(elem);
