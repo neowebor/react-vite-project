b1a8fb760da691295f087bebf24729421ea20ba5
neowebor <neowebor@gmail.com>
2024-11-24 20:23:18 +0200

    Add Fragment component usage example

--- src/main.jsx
+++ src/main.jsx
@@ -46,6 +46,25 @@ function MyFuncComponent() {
     return article;
 }
 
-const elem = React.createElement(MyFuncComponent)
+const classElem1 = React.createElement(MyClassComponent);
+const classElem2 = React.createElement(MyClassComponent);
 
-createRoot(document.getElementById('root')).render(elem);
+const funcElem1 = React.createElement(MyFuncComponent);
+const funcElem2 = React.createElement(MyFuncComponent);
+
+// Фрагмент - спеціальний компонент який дозволяє створювати обгорту для елементів яку реакт потім не буде малювати в верстці. пропси завжди мають бути null
+// const frag = React.createElement(React.Fragment, null, classElem1, classElem2, funcElem1, funcElem2)
+
+function App() {
+  const classElem1 = React.createElement(MyClassComponent);
+  const classElem2 = React.createElement(MyClassComponent);
+  
+  const funcElem1 = React.createElement(MyFuncComponent);
+  const funcElem2 = React.createElement(MyFuncComponent);
+  
+  return React.createElement('div', null, classElem1, classElem2, funcElem1, funcElem2)
+}
+
+const app = React.createElement(App);
+
+createRoot(document.getElementById('root')).render(app);
